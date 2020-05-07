8a2d989cd9bbdfd77a62d5d8e029eb3e21bb4d96
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-05-07 20:57:22 +0530

    add security conigurations

--- src/main/java/com/example/springjwt/HomeResource.java
+++ src/main/java/com/example/springjwt/HomeResource.java
@@ -1,21 +1,22 @@
 package com.example.springjwt;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeResource{
-    @GetMapping("/")
+    @GetMapping({"/"})
     public String home(){
         return ("<h1>Welcome</h1>");
     }
 
-    @GetMapping("/user")
+    @GetMapping({"/user"})
     public String user(){
         return ("<h1>Welcome USER</h1>");
     }
 
-    @GetMapping("/admin")
+    @GetMapping({"/admin"})
     public String admin(){
         return ("<h1>Welcome ADMIN</h1>");
     }
---
+++ src/main/java/com/example/springjwt/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.springjwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyUserDetailService userDetailService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService);
+    }
+}
